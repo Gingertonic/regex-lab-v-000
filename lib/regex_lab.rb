d050917d33024e3d44b62eb3ca8995e46eb5beda
Beth Schofield <bethmschofield@gmail.com>
2018-03-01 00:33:50 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,17 +1,18 @@
 def starts_with_a_vowel?(word)
-
+  word.match(/\A[aeiou]/i) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\bun\S*ing\b/)
+# => ["unassuming ambiguous understanding pomp circumstance uninteresting uncompromising"]
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.scan(/[A-Z]\A\W\z/)
 end
 
 def valid_phone_number?(phone)
